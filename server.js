d849df56b9c9a6e849ce3826373ae2bc589bd76f
Matthew C Wake <mcwake@outlook.com>
2018-07-20 14:06:23 +0100

    Fixed case issue with Heroku port assignment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const config = require('./config').getConfig();
 
 const app = express();
 
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
